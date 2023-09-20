7e655fc4a0e242f943987b3e9092ab9e33b05aec
Dmitrii.Kazantsev <dmitry.kazantsev@jetbrains.com>
2023-09-20 10:25:14 +0300

    AnotherUnityPackageCommit

---
+++ SampleScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class SampleScript : MonoBehaviour
+    {
+        //qqssQQQwwwwds
+        //qqssQQQwwwwds
+        //dasdada
+    }
+}
